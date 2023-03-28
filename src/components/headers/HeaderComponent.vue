78ac47d54bb24935f59658f80a7959612e436ffd
berdavoglio <berdavoglio@gmail.com>
2023-03-28 17:53:32 -0300

    Header structure almost ready

--- src/components/headers/HeaderComponent.vue
+++ src/components/headers/HeaderComponent.vue
@@ -5,11 +5,44 @@
         <p className="text-2xl">Bernardo Davoglio</p>
         <p className="">Programador</p>
       </div>
-      <div className="grid grid-cols-4 m-auto">
-        <div className="w-24">Home</div>
-        <div className="w-24">About</div>
-        <div className="w-24">Portfolio</div>
-        <div className="w-24">Contact</div>
+      <div className="grid grid-cols-4 m-auto h-12 cursor-pointer">
+        <div className="
+            w-24
+            border-r-2
+            hover:bg-gray-300
+            active:bg-gray-400
+            active:text-white
+            py-4
+            border-l-2">
+          Home
+        </div>
+        <div className="
+            w-24
+            border-r-2
+            hover:bg-gray-300
+            active:bg-gray-400
+            active:text-white
+            py-4">
+          About
+        </div>
+        <div className="
+            w-24
+            border-r-2
+            hover:bg-gray-300
+            active:bg-gray-400
+            active:text-white
+            py-4">
+          Portfolio
+        </div>
+        <div className="
+            w-24
+            border-r-2
+            hover:bg-gray-300
+            active:bg-gray-400
+            active:text-white
+            py-4">
+          Contact
+        </div>
       </div>
     </div>
     <div className="w-[95%] h-0.5 bg-black m-auto"></div>
