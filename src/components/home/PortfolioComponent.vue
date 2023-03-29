277229d92e34e3e881a42fa56a7478933c1983ab
berdavoglio <berdavoglio@gmail.com>
2023-03-29 16:41:20 -0300

    Create portfolio button to idicate to proper page

--- src/components/home/PortfolioComponent.vue
+++ src/components/home/PortfolioComponent.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="portfolio">
+    <div className="teste h-32 pt-12 text-2xl text-white cursor-pointer">
+      About my Portfolio? Click here and discover more about me!
+    </div>
   </div>
 </template>
 
@@ -10,3 +13,17 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+div.teste {
+  background: linear-gradient(124.09deg,
+      #D157F2 1.58%, #56308C 20.51%,
+      #5D60A6 77.34%, #1F1640 115.83%);
+}
+
+div.teste:hover {
+  background: linear-gradient(124.09deg,
+      #A042BA 1.58%, #381F5B 20.51%,
+      #33355B 77.34%, #0D091A 115.83%);
+}
+</style>
--- src/components/home/ShareComponent.vue
+++ src/components/home/ShareComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="share" className="h-[42rem] p-20">
+  <div class="share" className="h-[32rem] p-4">
     <div className="h-[28rem] w-[42rem] mx-auto rounded" style="
                 background: linear-gradient(124.09deg, #D157F2 1.58%, #56308C 20.51%,
               #5D60A6 77.34%, #1F1640 115.83%);"></div>
