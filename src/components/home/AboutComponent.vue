7ed15bcb78b79defe6c9183c24923b8f5b3c1007
berdavoglio <berdavoglio@gmail.com>
2023-03-29 16:00:37 -0300

    Add Read More Button in AboutComponent

--- src/components/home/AboutComponent.vue
+++ src/components/home/AboutComponent.vue
@@ -2,31 +2,38 @@
 <template>
   <div class="about">
     <div className="
-                      grid grid-cols-2
-                      bg-js_bg bg-center bg-contain bg-gray-500
-                      w-full h-[42rem]">
+                                grid grid-cols-2
+                                bg-js_bg bg-center bg-contain bg-gray-500
+                                w-full h-[42rem]">
       <div className="
-                      bg-ber bg-cover bg-center bg-no-repeat
-                      h-[85%] max-w-[60%]
-                      my-10 ml-[30%]
-                      rounded-3xl">
+                                bg-ber bg-cover bg-center bg-no-repeat
+                                h-[85%] max-w-[60%]
+                                my-10 ml-[30%]
+                                rounded-3xl">
       </div>
-      <p className="
-                  text-white text-justify
-                  max-w-[80%] max-h-[36rem]
-                  rounded-md
-                  mt-10 p-4
-                  bg-gray-900/75">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at eros id velit mattis sollicitudin. Suspendisse
-        dolor massa, rhoncus ac neque sit amet, suscipit vestibulum massa. Donec dictum justo tristique suscipit
-        pellentesque. Integer maximus aliquam eros, et venenatis sem molestie sit amet. Duis lacus nibh, laoreet et quam
-        in, tempor dapibus ligula. Vivamus sit amet laoreet neque. Duis et ullamcorper turpis, sit amet consectetur ex.
-        Vivamus tempus urna felis, vel malesuada arcu ullamcorper suscipit. Praesent at orci quis ipsum pharetra lacinia
-        eget convallis enim. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed feugiat, lorem eget pretium
-        lobortis, purus enim hendrerit felis, nec convallis lorem libero vel quam. Duis et nisi pellentesque, feugiat arcu
-        ac, tempor massa. Nulla gravida elit sed arcu imperdiet, blandit congue eros volutpat. Aliquam pulvinar accumsan
-        commodo. Vestibulum tristique eleifend egestas.
-      </p>
+      <div>
+        <p className="
+                            text-white text-justify
+                            max-w-[80%] min-h-[32rem]
+                            rounded-md
+                            mt-10 p-4
+                            bg-gray-900/75">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at eros id velit mattis sollicitudin. Suspendisse
+          dolor massa, rhoncus ac neque sit amet, suscipit vestibulum massa. Donec dictum justo tristique suscipit
+          pellentesque. Integer maximus aliquam eros, et venenatis sem molestie sit amet. Duis lacus nibh, laoreet et quam
+          in, tempor dapibus ligula. Vivamus sit amet laoreet neque. Duis et ullamcorper turpis, sit amet consectetur ex.
+          Vivamus tempus urna felis, vel malesuada arcu ullamcorper suscipit. Praesent at orci quis ipsum pharetra lacinia
+          eget convallis enim. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed feugiat, lorem eget
+          pretium
+          lobortis, purus enim hendrerit felis, nec convallis lorem libero vel quam. Duis et nisi pellentesque, feugiat
+          arcu
+          ac, tempor massa. Nulla gravida elit sed arcu imperdiet, blandit congue eros volutpat. Aliquam pulvinar accumsan
+          commodo. Vestibulum tristique eleifend egestas.
+        </p>
+        <div className="
+                w-32 bg-red-700 text-white p-2 mt-4 rounded-md hover:bg-red-800
+              active:bg-red-900 cursor-pointer">Read More</div>
+      </div>
     </div>
   </div>
 </template>
