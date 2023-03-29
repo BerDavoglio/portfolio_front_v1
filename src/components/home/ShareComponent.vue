832f103bb03f091947343e2fe06589e42a079534
berdavoglio <berdavoglio@gmail.com>
2023-03-29 16:00:55 -0300

    Fix height in ShareComponent

--- src/components/home/ShareComponent.vue
+++ src/components/home/ShareComponent.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="share">
-    <div className="h-[28rem] w-[42rem] mx-auto rounded " style="
+  <div class="share" className="h-[42rem] p-20">
+    <div className="h-[28rem] w-[42rem] mx-auto rounded" style="
                 background: linear-gradient(124.09deg, #D157F2 1.58%, #56308C 20.51%,
               #5D60A6 77.34%, #1F1640 115.83%);"></div>
     <div className="mb-12"></div>
