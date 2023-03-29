054df3da36fd91cdb49b40967589e9563c3f6197
berdavoglio <berdavoglio@gmail.com>
2023-03-29 17:16:05 -0300

    Creating new pages for page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import AboutView from '../views/AboutView.vue';
+import PortfolioView from '../views/PortfolioView.vue';
+import ContactView from '../views/ContactView.vue';
 
 const routes = [
   {
@@ -12,6 +14,16 @@ const routes = [
     path: '/about',
     name: 'about',
     component: AboutView,
+  },
+  {
+    path: '/portfolio',
+    name: 'portfolio',
+    component: PortfolioView,
+  },
+  {
+    path: '/contact',
+    name: 'contact',
+    component: ContactView,
   },
 ];
 
