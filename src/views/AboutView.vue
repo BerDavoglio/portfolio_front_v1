edc22c7e09cf51a0f9e3a2980441d4363673a69e
berdavoglio <berdavoglio@gmail.com>
2023-03-30 16:30:04 -0300

    Write more in AboutView (Incomplete)

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -4,29 +4,37 @@
       <Swiper>
         <SwiperSlide>
           <img src="../assets/images/ber_foto_1.jpeg" alt="" className="
-              object-cover
-              max-h-72 w-[60%]
-              m-auto rounded-3xl">
+                object-cover
+                max-h-[22rem] w-[60%]
+                m-auto rounded-3xl">
         </SwiperSlide>
         <SwiperSlide>
           <img src="../assets/images/ber_foto_2.jpeg" alt="" className="
-              object-cover
-              max-h-72 w-[60%]
-              m-auto rounded-3xl">
+                object-cover
+                max-h-[22rem] w-[60%]
+                m-auto rounded-3xl">
         </SwiperSlide>
         <SwiperSlide>
           <img src="../assets/images/ber_foto_3.jpeg" alt="" className="
-              object-cover
-              max-h-72 w-[60%]
-              m-auto rounded-3xl">
+                object-cover
+                max-h-[22rem] w-[60%]
+                m-auto rounded-3xl">
         </SwiperSlide>
         <SwiperSlide>
           <img src="../assets/images/ber_foto_4.jpg" alt="" className="
-              object-cover
-              max-h-72 w-[60%]
-              m-auto rounded-3xl">
+                object-cover
+                max-h-[22rem] w-[60%]
+                m-auto rounded-3xl">
         </SwiperSlide>
       </Swiper>
+    </div>
+    <div className="mx-[19rem]">
+      <div className="text-3xl font-bold text-left">Quem é?</div>
+      <div className="text-justify">Bernardo Davoglio, atualmente estudante de Engenharia Química
+        pela Universidade Federal do Paraná, é um desenvolvedor de Softwares, tanto Front-End
+        quanto Back-End, com um vasto conhecimento em diversas linguagens de programação, como
+        JavaScript e PHP.
+      </div>
     </div>
   </div>
 </template>
