f8cc93e9b0befe3630f5eb2e5aa09c5e2e16cbab
berdavoglio <berdavoglio@gmail.com>
2023-04-03 00:27:31 -0300

    Removed PartOne from Footer/2

--- src/components/footers/PartThree.vue
+++ src/components/footers/PartThree.vue
@@ -4,10 +4,6 @@
     <div class="text-2xl font-bold mb-3">
       {{ $t('footer.three.title') }}
     </div>
-    <div class="text-sm">
-      <strong>
-        {{ $t('footer.three.address') }}:
-      </strong></div>
     <div class="bg-white h-[1px] w-[80%] mx-auto my-3 rounded-md"></div>
     <div class="text-sm">
       <strong>{{ $t('footer.three.cellphone') }}: </strong>+55 41 9 8477-6477
